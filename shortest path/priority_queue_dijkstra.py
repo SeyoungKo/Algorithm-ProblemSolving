4f9e3ad121626f067f3109c5303b4879004cf4df
SeyoungKo <airseyoung@naver.com>
2021-04-28 08:53:08 +0900

    Modify : 우선순위 큐를 이용한 다익스트라 알고리즘

--- shortest path/priority_queue_dijkstra.py
+++ shortest path/priority_queue_dijkstra.py
@@ -6,49 +6,45 @@
 import heapq
 import sys
 
+INF = int(1e9)
 input = sys.stdin.readline
-INF = int(1e9) # 무한 설정
-
-if __name__ == '__main__':
-    # 노드의 개수, 간선의 개수
-    n, m = map(int, input().split())
-    # 시작 노드 번호
-    start = int(input())
-
-    # 각 노드에 연결되어 있는 노드 정보를 담는 리스트
-    graph = [[] for i in range(n+1)]  # 인덱스로 바로 노드에 접근할 수 있도록 n+1만큼 할당
-    # 최단 거리 테이블 초기화
-    distance = [INF] * (n+1)
-
-    # 모든 간선 정보 입력받기
-    for _ in range(m):
-        a, b, c = map(int, input().split()) # a번 노드에서 b번 노드로 가는 비용이 c
-        graph[a].append((b, c)) # a번 노드의 연결 정보 저장
-
 
 def dijkstra(start):
     q = []
-    heapq.heappush(q, (0, start)) # start 노드 초기화 (거리 0, start 노드 번호)
-    distance[start] = 0 # start 노드 최단거리 0으로 초기화
+    heapq.heappush(q, (0, start)) # 최단 경로(0), 노드 번호
+    distance[start] = 0
 
-    while q: # 큐가 비어있지 않다면
-        dist, target_n = heapq.heappop(q) # 가장 최단 거리, 노드 번호를 pop
-        if distance[target_n] < dist: # 이미 처리된 적 있는 노드라면 무시
+    while q:
+        dist, node = heapq.heappop(q) # 가장 dist(최단 거리)가 짧은 노드 순서대로 pop
+        if distance[node] < dist: # now 노드가 이미 처리된 적 있는 노드라면 무시
             continue
 
-        # 현재 노드와 연결된 인접한 노드들을 확인
-        for i in graph[target_n]:
+        for i in graph[node]: # now 노드와 연결된 다른 인접 노드 확인
             cost = dist + i[1]
-            if cost < distance[i[0]]: # 현재 노드를 거쳐 노드(i[0])로 이동하는 거리가 더 짧으면
-                distance[i[0]] = cost # 연결된 노드의 최단거리를 cost로 갱신한다.
+
+            if cost < distance[i[0]]: # distance[i[0]]: i[0]노드의 최단거리가 이미 저장되어 있는 상태
+                distance[i[0]] = cost # 저장된 최단거리보다 현재 노드를 거친 더 짧은 거리(cost)로 저장
                 heapq.heappush(q, (cost, i[0]))
 
-dijkstra(start) # 다익스트라 수행
 
-# 모든 노드로 가기 위한 최단 거리 출력
-for i in range(1, n+1):
-    if distance[i] == INF:
-        print('INFINITY')
-    else:
-        print(distance[i])
+if __name__ == '__main__':
+    print('n, m: ', end='')
+    n, m = map(int, input().split()) # 노드, 간선
 
+    print('start: ', end='')
+    start = int(input()) # 시작 노드 번
+
+    distance = [INF] * (n+1) # 노드의 최종 최단거리 저장 테이블
+    graph = [[] for _ in range(n+1)] # 그래프 초기화
+
+    for _ in range(m):
+        a, b, c = map(int, input().split()) # 노드 번호, 연결 노드, 거리
+        graph[a].append((b,c)) # 노드에 연결할 노드 번호와 거리를 저장
+
+    dijkstra(start)
+
+    for i in range(1, n+1):
+        if distance[i] == INF:
+            print('INFINITY!')
+        else:
+            print(distance[i])
