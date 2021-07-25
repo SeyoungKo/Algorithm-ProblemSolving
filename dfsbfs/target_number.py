7227dc471c2518f92f088f415c5178fb2bfad335
SeyoungKo <seyoungg1223@gmail.com>
2021-07-25 21:05:22 +0900

    Modify : 타겟 넘버
    solution() 함수 추가

--- dfsbfs/target_number.py
+++ dfsbfs/target_number.py
@@ -15,6 +15,31 @@ def dfs(prev, index, numbers, target):
 
     return ans
 
+def solution(numbers, target):
+    answer = 0
+
+    def dfs(prev, index, numbers, target):
+        answer = 0
+
+        if index >= len(numbers):
+            if target == prev:
+                return 1
+            return 0
+
+        cal1 = prev + numbers[index]
+        cal2 = prev - numbers[index]
+
+        answer += dfs(cal1, index + 1, numbers, target)
+        answer += dfs(cal2, index + 1, numbers, target)
+
+        return answer
+
+    current = numbers[0]
+    answer += dfs(current, 1, numbers, target)
+    answer += dfs(-current, 1, numbers, target)
+
+    return answer
+
 if __name__ == '__main__':
     numbers = [1, 1, 1, 1, 1]
     target = 3
