63abe9428b3081a7f45c93d0bef12408c912b676
SeyoungKo <seyoungg1223@gmail.com>
2021-07-30 20:06:26 +0900

    Add : Same Tree BFS

--- dfsbfs/same_tree.py
+++ dfsbfs/same_tree.py
@@ -1,5 +1,5 @@
 # Same Tree (100)
-
+from collections import deque
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -20,6 +20,27 @@ class Solution:
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
 
+class Solution2:
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        return True
+
+    def bfs(self, p, q):
+        queue = deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            if not self.isSameTree(p, q): # false
+                return False
+            if p or q:
+                queue.append((p.left, q.left))
+                queue.append((p.right, p.right))
+        return True
+
 if __name__ == '__main__':
     p = [1, 2, 3]
     q = [1, 2, 3]
@@ -29,4 +50,7 @@ if __name__ == '__main__':
 
     solution = Solution()
     print(solution.isSameTree(p_tree, q_tree))
+
+    solution2 = Solution2()
+    print(solution2.bfs(p_tree, q_tree))
 
