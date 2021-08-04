ee487a2b36551d262a84972a175904bd51b4925c
SeyoungKo <seyoungg1223@gmail.com>
2021-08-05 02:23:35 +0900

    Add : Binary Tree Inorder Traversal DFS

---
+++ dfsbfs/binary_tree_inorder_traversal.py
@@ -0,0 +1,38 @@
+# Binary Tree Inorder Traversal (94)
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root):
+        result = []
+        stack = []
+
+        while True:
+            # left를 모두 stack에 append
+            while root:
+                stack.append(root)
+                root = root.left
+
+            if not stack:
+                return result
+
+            node = stack.pop()
+            result.append(node.val)
+            # left 모두 탐색 후 right를 모두 stack에 append
+            root = node.right
+
+if __name__ == '__main__':
+    # root = [1, None, 2, 3]
+
+    root = TreeNode(1)
+    root.left = TreeNode(None)
+    root.right = TreeNode(2)
+    root.right.left = TreeNode(3)
+
+    s = Solution()
+    print(s.inorderTraversal(root))
