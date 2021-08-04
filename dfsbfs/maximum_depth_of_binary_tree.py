06bd6afd8b484cdaed9bb49e322c60ca7441f4c9
SeyoungKo <seyoungg1223@gmail.com>
2021-08-05 00:00:21 +0900

    Modify : Maximum Depth of Binary Tree DFS
    이진트리 DFS 재귀 함수 버전 추가

--- dfsbfs/maximum_depth_of_binary_tree.py
+++ dfsbfs/maximum_depth_of_binary_tree.py
@@ -12,16 +12,25 @@ class Solution:
     def maxDepth(self, root):
         stack = []
         if root is not None:
-            stack.append((1, root)) # depth, object
+            stack.append((1, root))
 
         while stack:
-            cur_depth, root = stack.pop()
-            print(f'root is {root}')
-            if root is not None:
-                stack.append((cur_depth + 1, root.left))
-                stack.append((cur_depth + 1, root.right))
+            cur_depth, node = stack.pop()
+            if node is not None:
+                stack.append((cur_depth + 1, node.left))
+                stack.append((cur_depth + 1, node.right))
 
         return cur_depth
+
+    # DFS Recursion
+    def dfs(self, root):
+        if root is None:
+            return 0
+        left_depth = self.dfs(root.left)
+        right_depth = self.dfs(root.right)
+
+        max_depth = (max(left_depth, right_depth) + 1)
+        return max_depth
 
 if __name__ == '__main__':
     # root = [3, 9, 20, None, None, 15, 7]
@@ -33,4 +42,5 @@ if __name__ == '__main__':
     root.right.left = TreeNode(15)
     root.right.right = TreeNode(7)
 
-    print(s.maxDepth(root))
+    # print(s.maxDepth(root))
+    print(s.dfs(root))
