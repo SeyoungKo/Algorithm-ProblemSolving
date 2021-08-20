81d79580f890ec36f4b5854964326d295a0626ea
SeyoungKo <seyoungg1223@gmail.com>
2021-08-20 23:24:55 +0900

    Add : Heap Delete
    힙 삭제

--- datastructure/heap.py
+++ datastructure/heap.py
@@ -34,9 +34,22 @@ class Heap:
 
         return True
 
+    # 루트 노드 값이 자식 노드보다 작을 경우 자식 노드 중 가장 큰 값과 루트 노드 위치를 바꿔주는 작업을 반복
+    def move_down(self, poped_idx):
+        pass
+
     def delete(self):
         # 최상단 노드 (root 노드)를 삭제하는 것이 일반적
-        pass
+        if len(self.heap_arr) <= 1:
+            return None
+        pop_root = self.heap_arr[1] # 최상단 노드를 삭제
+        self.heap_arr[1] = self.heap_arr[-1] # 가장 하위에 있는 노드를 루트 위치로 옮기기
+        poped_idx = 1
+
+        while self.move_down(poped_idx): # 루트 노드 값이 자식 노드보다 작을 경우 자식 노드 중 가장 큰 값과 루트 노드 위치를 바꿔주는 작업을 반복
+            pass
+
+        return pop_root
 
 if __name__ == '__main__':
     heap = Heap(15)
