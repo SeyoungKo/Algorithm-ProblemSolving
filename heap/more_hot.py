18131028f8497d95b9cbc2c0883a8095b653f440
SeyoungKo <seyoungg1223@gmail.com>
2021-08-24 10:22:17 +0900

    Add : 더 맵게
    최소 힙 문제 풀이

---
+++ heap/more_hot.py
@@ -0,0 +1,24 @@
+# 더 맵게
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while True:
+        min_val1 = heapq.heappop(scoville)
+        if min_val1 >= K:
+            break
+        if len(scoville) <= 0:
+            return -1
+        min_val2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, min_val1 + min_val2 * 2)
+        answer += 1
+    return answer
+
+
+if __name__ == '__main__':
+    scoville = [1, 2, 3, 9, 10, 12]
+    K = 7
+    print(solution(scoville, K))
