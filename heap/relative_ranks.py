7e262bb4d8e54c2ae809b290db5117ea1022caa2
SeyoungKo <seyoungg1223@gmail.com>
2021-08-31 21:38:14 +0900

    Add : Relative Ranks
    leetcode heap, 정렬 문제풀이 방식 추가

--- heap/relative_ranks.py
+++ heap/relative_ranks.py
@@ -1,9 +1,27 @@
 # Relative Ranks (506)
-
 import heapq
 
+
 class Solution:
-    def findRelativeRanks(self, score):
+    def findRelativeRanks_sort(self, score):
+        g = sorted(score, reverse=True)
+        d = {}
+        m = []
+
+        for i in range(len(g)):
+            if i == 0:
+                d[g[i]] = "Gold Medal"
+            elif i == 1:
+                d[g[i]] = "Silver Medal"
+            elif i == 2:
+                d[g[i]] = "Bronze Medal"
+            else:
+                d[g[i]] = str(i + 1)
+        for i in score:
+            m.append(d[i])
+        return m
+
+    def findRelativeRanks_heap(self, score):
         heap = []
         heapq.heapify(heap)
 
@@ -28,8 +46,10 @@ class Solution:
 
         return res
 
-
 if __name__ == '__main__':
-    score = [5, 4, 3, 2, 1]
+    # score = [5, 4, 3, 2, 1]
+    score = [10,3,8,9,4]
     s = Solution()
-    print(s.findRelativeRanks(score))
+
+    print(s.findRelativeRanks_sort(score))
+    print(s.findRelativeRanks_heap(score))
