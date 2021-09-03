3167c771a01e2eeb87fee17b7dc3d24bd3510828
SeyoungKo <seyoungg1223@gmail.com>
2021-09-03 19:34:06 +0900

    Modify : Selection Sort
    선택 정렬 구현 수정

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -1,20 +1,19 @@
-# 선택정렬 O(N**)
+# 선택정렬 O(n²)
+# 최소값을 선택!
 # 1. 가장 작은 수를 골라 맨 앞에 놓인 수와 비교해 바꾼다
 # 2. 첫번째 인덱스 수를 제외한 나머지 중에서 가장 작은 수를 골라 두번째 인덱스 수와 비교해 바꾼다
 # 3. n-1번 반복한다
 # ->특정 리스트에서 가장 작은 데이터를 찾을 때 주로 사용된다. (데이터 수 많은 정렬 비효율적)
 
-def selection_sort():
-    arr = [7, 5, 1, 2, 5, 3, 9, 12, 11]
-
-    for i in range(len(arr)):
-        min_idx = i # 초기상태
-        # i를 제외한 나머지 배열 값들 중 가장 작은 값 탐색
-        for j in range(len(arr)-1, i, -1):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    print(arr)
+def selection_sort(data):
+    for base in range(len(data) - 1):
+        min_idx = base
+        for i in range(base + 1, len(data)):
+            if data[min_idx] > data[i]:
+                min_idx = i
+        data[min_idx], data[base] = data[base], data[min_idx]
+    return data
 
 if __name__ == '__main__':
-    selection_sort()
+    arr = [7, 5, 1, 2, 5, 3, 9, 12, 11]
+    print(selection_sort(arr))
