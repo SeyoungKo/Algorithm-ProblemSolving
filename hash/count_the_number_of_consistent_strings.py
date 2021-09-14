326e608d1a087268e50421dc405aa95846054645
SeyoungKo <seyoungg1223@gmail.com>
2021-09-14 23:26:21 +0900

    Add : Count the Number of Consistent Strings
    leetcode hash table 문제 풀이

---
+++ hash/count_the_number_of_consistent_strings.py
@@ -0,0 +1,26 @@
+# Count the Number of Consistent Strings (1684)
+
+class Solution:
+    def countConsistentStrings(self, allowed, words):
+        '''
+        :param allowed: string
+        :param words: List
+        :return: integer
+        '''
+        allowed = set(allowed)
+        cnt = 0
+
+        for word in words:
+            for letter in word:
+                if letter not in allowed:
+                    cnt += 1
+                    break
+
+        return len(words) - cnt
+
+if __name__ == '__main__':
+    allowed = "abc"
+    words = ["a", "b", "c", "ab", "ac", "bc", "abc"]
+
+    s = Solution()
+    print(s.countConsistentStrings(allowed, words))
