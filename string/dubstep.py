edf63fee1137d2776621182e39047ecf3b81cfc2
SeyoungKo <seyoungg1223@gmail.com>
2021-10-11 00:01:24 +0900

    Add : Dubstep
    codewars 정규표현식 문자열 문제 풀이

---
+++ string/dubstep.py
@@ -0,0 +1,16 @@
+# Dubstep
+import re
+
+class Solution:
+    def song_decoder(self, song):
+        '''
+        :param song: str
+        :return: str
+        '''
+        # (문자열)+ : 특정 문자열 패턴 n번 반복
+        return re.sub(r'(WUB)+', ' ', song).strip()
+
+if __name__ == '__main__':
+    strs = 'WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'
+    s = Solution()
+    print(s.song_decoder(strs))
