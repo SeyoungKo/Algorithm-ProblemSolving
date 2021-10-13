3e91925ce42e54cb29c6efcae19cb91f628de92f
SeyoungKo <seyoungg1223@gmail.com>
2021-10-13 23:28:22 +0900

    Add : Valid Parentheses
    codewars 문자열 문제 풀이

---
+++ string/valid_parentheses.py
@@ -0,0 +1,24 @@
+# Valid Parentheses
+
+class Solution:
+    def valid_parentheses(self, strs):
+        '''
+        :param strs: str
+        :return: bool
+        '''
+        
+        left, right = 0, 0
+        for s in strs:
+            if right > left:
+                return False
+            if s == '(':
+                left += 1
+            elif s == ')':
+                right += 1
+
+        return True if left == right else False
+
+if __name__ == '__main__':
+    strs = ")(()))"
+    s = Solution()
+    print(s.valid_parentheses(strs))
