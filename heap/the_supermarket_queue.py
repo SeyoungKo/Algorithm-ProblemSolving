eb550da9479e5242be6ea5fcce6808ed94b4e7f8
SeyoungKo <seyoungg1223@gmail.com>
2021-10-17 23:16:25 +0900

    Add : The Supermarket queue
    codewars heap 문제 풀이

---
+++ heap/the_supermarket_queue.py
@@ -0,0 +1,21 @@
+# The Supermarket Queue
+
+class Solution:
+    def queue_time(self, customers, n):
+        '''
+        :param list:
+        :param int:
+        :return: int
+        '''
+        
+        l = [0] * n
+        for i in customers:
+            l[l.index(min(l))] += i
+        return max(l)
+
+if __name__ == '__main__':
+    customers = [1, 2, 3, 4, 5]
+    n = 1
+
+    s = Solution()
+    print(s.queue_time(customers, n))
