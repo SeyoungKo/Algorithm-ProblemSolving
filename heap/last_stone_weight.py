bfad4284fd7d3be809512c2712ea069ab09f97e4
SeyoungKo <seyoungg1223@gmail.com>
2023-04-26 23:15:40 +0900

    Add : last stone weight

--- heap/last_stone_weight.py
+++ heap/last_stone_weight.py
@@ -1,18 +1,20 @@
 # Last Stone Weight (1046)
-
+# x == y : 둘다 삭제
+# x != y : x는 없어지고 y-x한 값만 남음
 import heapq
 
 class Solution:
     def lastStoneWeight(self, stones):
-        stones = [-stone for stone in stones]
+        stones = [-stone for stone in stones] # 최대 힙으로 만들기
         heapq.heapify(stones)
 
         while len(stones) > 1:
-            first_node = abs(heapq.heappop(stones))
-            second_node = abs(heapq.heappop(stones))
-            if first_node != second_node:
-                heapq.heappush(stones, -abs(first_node - second_node))
-        return abs(stones[0]) if len(stones) >= 1 else 0
+            top = abs(heapq.heappop(stones))
+            top2 = abs(heapq.heappop(stones))
+            if top != top2:
+                heapq.heappush(stones, abs(top2 - top))
+        return stones[0] if len(stones) >= 1 else 0
+
 
 if __name__ == '__main__':
     s = Solution()
