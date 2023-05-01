f70198063003f63b22107da884c9b6e0f7335283
SeyoungKo <seyoungg1223@gmail.com>
2023-05-01 22:40:21 +0900

    Add : target number

--- dfsbfs/target_number.py
+++ dfsbfs/target_number.py
@@ -1,52 +1,20 @@
-# 타겟 넘버
-def dfs(prev, index, numbers, target):
-    ans = 0
-    if index >= len(numbers):
+def target_number(idx, prev, numbers, target):
+    if idx >= len(numbers):
         if target == prev:
             return 1
         return 0
 
-    cur1 = prev + numbers[index]
-    cur2 = prev - numbers[index]
+    sum1 = prev + numbers[idx]
+    sum2 = prev - numbers[idx]
 
-    # +, - 두가지 경우의 수
-    ans += dfs(cur1, index+1, numbers, target)
-    ans += dfs(cur2, index+1, numbers, target)
-
-    return ans
-
-def solution(numbers, target):
-    answer = 0
-
-    def dfs(prev, index, numbers, target):
-        answer = 0
-
-        if index >= len(numbers):
-            if target == prev:
-                return 1
-            return 0
-
-        cal1 = prev + numbers[index]
-        cal2 = prev - numbers[index]
-
-        answer += dfs(cal1, index + 1, numbers, target)
-        answer += dfs(cal2, index + 1, numbers, target)
-
-        return answer
-
-    current = numbers[0]
-    answer += dfs(current, 1, numbers, target)
-    answer += dfs(-current, 1, numbers, target)
-
-    return answer
+    return target_number(idx + 1, sum1, numbers, target) + target_number(idx + 1, sum2, numbers, target)
 
 if __name__ == '__main__':
     numbers = [1, 1, 1, 1, 1]
     target = 3
-    answer = 0
+    result = 0
 
-    current = numbers[0]
-    answer += dfs(current, 1, numbers, target)
-    answer += dfs(-current, 1, numbers, target)
+    result += target_number(1, numbers[0], numbers, target)
+    result += target_number(1, -numbers[0], numbers, target)
 
-    print(answer)
+    print(result)
