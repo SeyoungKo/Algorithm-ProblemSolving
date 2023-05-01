a8f3935fd119781276c95bbd3e65f64ec0e6ce02
SeyoungKo <seyoungg1223@gmail.com>
2023-05-01 22:39:38 +0900

    Add : save area

---
+++ dfsbfs/save_area.py
@@ -0,0 +1,25 @@
+# 영역 구하기 (2583번)
+import sys
+
+def dfs(graph, x, y):
+    if x <= -1 or y <= -1 or x >= n or y >= m:
+        return False
+    if graph[x][y] ==
+
+if __name__ == '__main__':
+    n, m, k = list(map(int, sys.stdin.readline().split(' ')))
+    graph = []
+
+    for _ in range(k):
+        v1, v2 = list(map(int, sys.stdin.readline().split(' ')))
+        v3, v4 = list(map(int, sys.stdin.readline().split(' ')))
+
+        # 직사각형이 존재하는 영역을 True로 변경
+        for i in range():
+            pass
+        for j in range():
+            pass
+
+        # 동서남북 이동
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
