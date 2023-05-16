bf137084ee70ea78ed33078d8c3ba556ce39f6a7
SeyoungKo <seyoungg1223@gmail.com>
2023-05-16 22:53:55 +0900

    Update multiply_plus.py

--- greedy/multiply_plus.py
+++ greedy/multiply_plus.py
@@ -1,7 +1,9 @@
 # 곱하기 혹은 더하기
 # 왼쪽부터 오른쪽 방향으로 숫자를 확인해 X 혹은 +로 만들 수 있는 가장 큰 수를 구해본다.
+
 if __name__ == '__main__':
     print('num: ', end='')
+    
     num = list(map(int, input()))
     result = 0
 
@@ -10,4 +12,5 @@ if __name__ == '__main__':
             result += num[i]
         else:
             result *= num[i]
+            
     print(result)
